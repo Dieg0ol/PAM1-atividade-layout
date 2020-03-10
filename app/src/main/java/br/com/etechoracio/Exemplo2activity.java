0b0bc318662608a708431c729c82cb6495f70d01
Diego <diegocaboclo@hotmail.com>
2020-03-09 22:27:13 -0300

    alinhamento

---
+++ app/src/main/java/br/com/etechoracio/Exemplo2activity.java
@@ -0,0 +1,14 @@
+package br.com.etechoracio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Exemplo2activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exemplo2activity);
+    }
+}
